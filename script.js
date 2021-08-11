6e9fecb84e552b500657fc8986e84bffa1cdcb94
Katherine Burr <katherinevb@hotmail.co.uk>
2021-08-11 16:28:38 +0100

    UI finished

--- script.js
+++ script.js
@@ -1,14 +1,66 @@
+// Variables
+const displayH2 = document.querySelector(".displayString");
+const runningTotalDisplay = document.querySelector(".running-total");
+const btns = document.querySelectorAll(".btn");
+const playerTotalP = document.querySelector("#player-total");
+const computerTotalP = document.querySelector("#computer-total");
+const popUp = document.querySelector(".popup");
+const resetBtn = document.querySelector("#reset");
+const rockIcon = document.querySelector(".hand-rock");
+const paperIcon = document.querySelector(".hand-paper");
+const scissorsIcon = document.querySelector(".hand-scissors");
+let playerSelection;
+let computerSelection;
+let playerTotal = 0;
+let computerTotal = 0;
+let noWinner = true;
 let play;
 let resultString;
+// Event listeners
+btns.forEach((btn) => btn.addEventListener("click", updateGame));
+playerTotalP.addEventListener("transitionend", removePink);
+computerTotalP.addEventListener("transitionend", removePink);
+rockIcon.addEventListener("transitionend", removePink);
+paperIcon.addEventListener("transitionend", removePink);
+scissorsIcon.addEventListener("transitionend", removePink);
+//Functions
+
+function updateGame(e) {
+  playerSelection = e.target.attributes[0].value;
+  computerSelection = computerPlay();
+  playRound(computerSelection, playerSelection);
+  displayH2.innerHTML = resultString;
+  if (resultString.includes("Winner")) {
+    playerTotal++;
+    playerTotalP.classList.add("pink");
+  } else if (resultString.includes("Loser")) {
+    computerTotal++;
+    computerTotalP.classList.add("pink");
+  }
+  playerTotalP.textContent = playerTotal;
+  computerTotalP.textContent = computerTotal;
+  if (playerTotal === 5 || computerTotal === 5) {
+    const winnerString = document.querySelector(".winnerString");
+    const winnerDetails = document.querySelector(".winnerDetails");
+    if (playerTotal > computerTotal) {
+      winnerString.textContent = "Winner!";
+    } else {
+      winnerString.textContent = "Loser!";
+    }
+    winnerDetails.textContent = `You won ${playerTotal} and the computer won ${computerTotal}.`;
+    popUp.classList.add("active");
+    resetBtn.addEventListener("click", () => resetGame());
+  }
+}
 
 function computerPlay() {
   let randomNumber = Math.floor(Math.random() * 3) + 1;
   if (randomNumber === 1) {
-    play = "Rock";
+    play = "rock";
   } else if (randomNumber === 2) {
-    play = "Paper";
+    play = "paper";
   } else {
-    play = "Scissors";
+    play = "scissors";
   }
   return play;
 }
@@ -18,60 +70,43 @@ function playRound(computerSelection, playerSelection) {
   let computerPlay = computerSelection.toLowerCase();
   if (computerPlay != playerPlay) {
     if (computerPlay === "rock" && playerPlay === "scissors") {
-      resultString = `You lose! Rock beats scissors.`;
+      resultString = `<span>Loser!</span> Rock beats scissors.`;
+      rockIcon.classList.add("pink");
     } else if (computerPlay === "paper" && playerPlay === "rock") {
-      resultString = `You lose! Paper beats rock.`;
+      resultString = `<span>Loser!</span> Paper beats rock.`;
+      paperIcon.classList.add("pink");
     } else if (computerPlay === "scissors" && playerPlay === "paper") {
-      resultString = `You lose! Scissors beat paper.`;
+      resultString = `<span>Loser!</span> Scissors beat paper.`;
+      scissorsIcon.classList.add("pink");
     } else if (computerPlay === "rock" && playerPlay === "paper") {
-      resultString = `You win! Paper beats rock.`;
+      resultString = `<span>Winner!</span> Paper beats rock.`;
+      paperIcon.classList.add("pink");
     } else if (computerPlay === "paper" && playerPlay === "scissors") {
-      resultString = `You win! Scissors beat paper.`;
+      resultString = `<span>Winner!</span> Scissors beat paper.`;
+      scissorsIcon.classList.add("pink");
     } else {
-      resultString = `You win! Rock beats scissors!`;
+      resultString = `<span>Winner!</span> Rock beats scissors!`;
+      rockIcon.classList.add("pink");
     }
   } else {
     resultString = `It's a draw - ${computerPlay} and ${playerPlay}.`;
+    if (computerPlay === "rock") {
+      rockIcon.classList.add("pink");
+    } else if (computerPlay === "paper") {
+      paperIcon.classList.add("pink");
+    } else {
+      scissorsIcon.classList.add("pink");
+    }
   }
   return resultString;
 }
-
-function game() {
-  let playerScore = 0;
-  let computerScore = 0;
-  let i = 0;
-
-  let playerSelection = prompt("Select rock, paper or scissors: ").toLowerCase();
-  let isNotVaild = true;
-  while (isNotVaild) {
-    if (playerSelection != "rock" && playerSelection != "paper" && playerSelection != "scissors") {
-      alert("Invalid choice, choose again.");
-      playerSelection = prompt("Select rock, paper or scissors: ").toLowerCase();
-      isNotVaild = true;
-    } else {
-      isNotVaild = false;
-    }
-  }
-  let computerSelection = computerPlay();
-  let round = playRound(computerSelection, playerSelection);
-  console.log(round);
-  if (round.includes("You win")) {
-    playerScore++;
-  } else if (round.includes("You lose")) {
-    computerScore++;
-  }
-
-  let winner;
-  if (playerScore < computerScore) {
-    winner = "the computer";
-  } else if (playerScore > computerScore) {
-    winner = "you";
-  }
-  console.log(
-    `You won ${playerScore} games, the computer won ${computerScore}. ${
-      winner ? `The winner is ${winner}.` : ""
-    }`
-  );
+function resetGame() {
+  popUp.classList.remove("active");
+  playerTotal = 0;
+  computerTotal = 0;
+  playerTotalP.textContent = playerTotal;
+  computerTotalP.textContent = computerTotal;
 }
-
-//game();
+function removePink(e) {
+  e.target.classList.remove("pink");
+}
